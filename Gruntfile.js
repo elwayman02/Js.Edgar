ebecd6820eb60a921119b7455b3637ad4e03d082
Jordan Hawker <hawker.jordan@gmail.com>
2015-03-13 12:04:25 -0700

    Add grunt-cli and npm test config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,17 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    exec: {
-      tests: './node_modules/qunit-cli/bin/qunit-cli ./tests/tests.js'
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        exec: {
+            testsci: './node_modules/qunit-cli/bin/qunit-cli ./tests/tests.js',
+            tests: 'qunit-cli tests/tests.js'
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-exec');
+    grunt.loadNpmTasks('grunt-exec');
 
-  grunt.registerTask('test', 'Run Tests', ['exec:tests']);
+    grunt.registerTask('test', 'Run Tests', ['exec:tests']);
+    grunt.registerTask('testci', 'Run Tests', ['exec:testsci']);
 
 };
