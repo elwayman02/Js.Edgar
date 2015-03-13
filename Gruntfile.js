f0507d60a5c6bf089972387539aa831ef383f027
Vidhur Vohra <vvohra@yahoo-inc.com>
2015-03-13 01:19:25 -0700

    [FEATURE] Adding Grunt test task
    Removing uglify

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    exec: {
+      tests: './node_modules/qunit-cli/bin/qunit-cli ./tests/tests.js'
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-exec');
+
+  // Default task(s).
+  grunt.registerTask('test', 'Run Tests', ['exec:tests']);
+
+};
